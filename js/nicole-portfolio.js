d7425cdc54f4ff40c88aa850aeac08fe2f90e274
Thomas Schmidt <schmidt@netaction.de>
2014-08-22 09:58:02 +0200

    Übertrieben viele Fontawesome-Icons!

--- js/nicole-portfolio.js
+++ js/nicole-portfolio.js
@@ -1,4 +1,16 @@
 $(function() {
 
+	// Smooth scrolling to anchors
+	// https://stackoverflow.com/questions/14804941
+	$(".navbar-nav a[href^='#']").on('click', function(e) {
+		e.preventDefault();
+		var hash = this.hash;
+		$('html, body').animate({
+			scrollTop: $(this.hash).offset().top
+		}, 300, function(){
+			window.location.hash = hash;
+		});
+	});
+
 });
 
